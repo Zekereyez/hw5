e5486a545a1e0b432182757a249c03203259dea0
Ezequiel Reyes <reyeseze@usc.edu>
2022-12-02 01:52:46 +0000

    working wordle

--- wordle.cpp
+++ wordle.cpp
@@ -4,6 +4,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
@@ -12,6 +13,9 @@ using namespace std;
 // Add prototypes of helper functions here
 set<string> wordleHelper(string& alphabet, string floating, string& word, set<string> psol, int n);
 bool wordIsReady(string& word, string floating);
+void findWords(string alphabet, string& word, string floating, set<string>& psol, int pos);
+void tryLetter(string alphabet, string& word, string floating, set<string>& psol, int pos);
+void insertWordsWithFloating(set<string>& psol, string floating, set<string>& realWords);
 /**
  * @brief Returns the list of all legal words in the provided dictionary
  *        that meet the criteria provided in the `in` and `floating`
@@ -30,32 +34,27 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-    // recursive backtracking type thang here so what I need to do is 
-    // write a function to compute all possible words that exist given 
-    // a string containing the already guessed letters that are IN THE 
-    // CORRECT LOCATION
-
-    // the FLOATING string is a string of letters that MUST appear in the 
-    // word somewhere 
-
-    // we return set of all words
-
-    // okay so we can do like two for loops here that will backtrack
-    // for the solution and reaches the length of size n ig 
-    // we can get the length of the word by the length of the in 
-    int n = in.length();
     // pass this off
     string word = in;
     set<string> realWords;
     set<string> psol;
     string alpha = "abcdefghijklmnopqrstuvwxyz";
-    psol = wordleHelper(alpha, floating, word, psol, n);
-    // once we have the set of possible solutions we can check them with 
-    // the dictionary and from there we polish and cut off and return only 
-    // valid words from the recursive solution
-    // set<string>::iterator jt = 
-    cout << "entered the wordle" << endl;
-    set<string>::iterator it = psol.begin();
+    int pos = 0;
+    // cout << "entering find words" << endl;
+    unsigned int dashes = 0;
+    for (auto letter : in) {
+      if (letter == '-') {
+        dashes++;
+      }
+    }
+    if (dashes == floating.size()) {
+      findWords(floating, word, floating, psol, pos);
+    }
+    else findWords(alpha, word, floating, psol, pos);
+    // cout << "entered the wordle" << endl;
+    // insertWordsWithFloating(psol, floating, realWords);
+    // cout << "back in wordle" << endl;
+     set<string>::iterator it = psol.begin();
     for (; it != psol.end(); ++it) {
       string currWord = *it;
       if (dict.find(currWord) != dict.end()) {
@@ -66,56 +65,54 @@ std::set<std::string> wordle(
 }
 
 // Define any helper functions here
-set<string> wordleHelper(string& alphabet, string floating, string& word, set<string> psol, int n) {
-    // base case here for string of length n 
-    cout << "Current Word:" << word << endl;
-    if (wordIsReady(word, floating)) {
-      cout << word << endl;
-      psol.insert(word);
-    }
-    // test 2
-    // recursive case for finding solution 
-    else {
-        // try letters in the floating one at a time 
-        for (unsigned int i = 0; i < word.size(); ++i) {
-            // make sure we can make changes to correct location
-            if (word[i] != '-') {
-                break;
-            }
-            for (unsigned int j = 0; j < alphabet.size(); ++j) {
-                // here is where we would insert and change the word?
-                // issue here is that with the - tags in the word this 
-                // means the word would trigger the base case since it 
-                // matches the word here... so what to do.
-                word[i] = floating[j];
-                // recursive call again 
-                wordleHelper(alphabet, floating, word, psol, n);
-            }
-        }
-    }
-    return psol;
-}
 
 // Helper function for determining if word is only filled with alphabet
-bool wordIsReady(string& word, string floating) {
-    for (unsigned int i = 0; i < word.size(); ++i) {
-      // short circuit here
-        if (word[i] == '-') {
-            return false;
+void findWords(string alphabet, string& word, string floating, set<string>& psol, int pos) {
+  // base case 
+  // cout << "in find words" <<endl;
+  // cout << "Word: " << word << endl;
+  if (pos == word.size()) {
+    // we have now a complete word that needs to be checked if it 
+    // has the floating letters in it 
+    for (auto letter : word) {
+      for (unsigned int i = 0; i < floating.size(); ++i) {
+        if (letter == floating[i]) {
+          // we delete the letter from floating 
+          floating.erase(floating.begin() + i);
         }
-        // word is ready will only get called when the word is full so
-        // we can iterate and check if the word matches all the floating words
-        for (unsigned int j = 0; j < floating.size(); ++j) {
-          if (word[i] == floating[j]) {
-            // delete the floating letters at pos j
-            floating.erase(floating.begin() + j);
-          }
-        }
+      }
     }
-    if (floating.size() == 0) {
-      return true;
+    // // at the end of looping here we need to check and see if the floating string
+    // // is empty, if so then word is valid
+    if (floating == "") {
+      psol.insert(word);
     }
-    else {
-      return false;
-    }
+    return;
+  }
+  // we try a letter if we are at a "-" pos in the letter
+  if (word[pos] == '-') {
+    // cout << "entered if for tryLetter" << endl;
+    tryLetter(alphabet, word, floating, psol, pos);
+  }
+  else {
+    pos++;
+    findWords(alphabet, word, floating, psol, pos);
+  }
+}
+
+void tryLetter (string alphabet, string& word, string floating, set<string>& psol, int pos) {
+  // this is where the recursive magic is going to happen. 
+  // we should only enter this function if we know we can try 
+  // a word. I'm tired of this whole checking bullshit atp 
+
+  for (unsigned int i = 0; i < alphabet.size(); ++i) {
+    // so what i do here is just try a letter at the current location and 
+    // recursively call the findWords function with the updated pos
+    word[pos] = alphabet[i];
+    pos++;
+    findWords(alphabet, word, floating, psol, pos);
+    pos--;
+  }
+  // if no found then go back to what pos and space was before
+  word[pos] = '-';
 }
